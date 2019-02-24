40b1f57ba6af4b77bf0907308690fc70f30c6515
Brendon Page <brendonpage@live.co.za>
2019-02-24 19:10:23 +0200

    Prototype of tile layout working in simulator, just need to check on device now

--- app/index.js
+++ app/index.js
@@ -1,14 +1,22 @@
 import * as Honeycomb from "../libs/honeycomb/honeycomb";
+import document from "document";
 
 const Hex = Honeycomb.extendHex({
-    size: 30,           // default: 1
-    orientation: 'flat' // default: 'pointy'
+    size: 34.6,
+    orientation: "pointy"
 });
 
 const Grid = Honeycomb.defineGrid(Hex);
-const grid = Grid.rectangle({ width: 4, height: 4 });
+const grid = Grid.rectangle({ width: 7, height: 6 });
 
 grid.forEach(hex => {
     const point = hex.toPoint();
-    console.log(`${point.x} ${point.y} ${hex.x} ${hex.y}`);
+    const id = `${hex.x}${hex.y}`;
+
+    const image = document.getElementById(id)
+    image.href = `Tiles/Terrain/Dirt/dirt_0${hex.x + 2}.png`
+    image.width = 60;
+    image.height = 70;
+    image.x = point.x - (image.width / 2);
+    image.y = point.y + (Math.sqrt(34.5) - (image.width / 2));
 });
