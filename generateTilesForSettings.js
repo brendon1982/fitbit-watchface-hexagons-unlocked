09dee7e86645982d4b4fca7a0525bbf58cdd4861
Brendon Page <brendonpage@live.co.za>
2019-03-06 21:30:18 +0200

    Resized the tiles so that they are closer to the actual usage size.

--- generateTilesForSettings.js
+++ generateTilesForSettings.js
@@ -1,16 +1,27 @@
 require = require("esm")(module)
 const datauri = require("datauri").sync;
-const availableTiles = require("./resources/tiles").availableTiles;
+const availableTiles = require("./resources/availableTiles").availableTiles;
 const fs = require("fs");
+const jimp = require("jimp");
 
-availableTiles.forEach(tile => {
-    tile.image = datauri(`.${tile.image}`)
+jimp.read("./settingsTileTemplate.png").then(template => {
+    const promises = availableTiles.map(tile => {
+        return jimp.read(`.${tile.image}`).then(image => {
+            return template.composite(image, 5, 0).getBase64Async(image.getMIME())
+                .then(base64 => {
+                    tile.image = base64;
+                });
+        });
+    });
+
+    Promise.all(promises)
+        .then(() => {
+            const data = `const availableTiles = ${JSON.stringify(availableTiles)};
+
+            export {
+                availableTiles
+            }`;
+
+            fs.writeFileSync("./settings/tilesWithEmbeddedImages.js", data);
+        });
 });
-
-const data = `const availableTiles = ${JSON.stringify(availableTiles)};
-
-export {
-    availableTiles
-}`;
-
-fs.writeFileSync("./settings/tilesWithEmbeddedImages.js", data);
