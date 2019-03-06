c7ed0be622e676682118e0835fd41917d45a8bfc
Brendon Page <brendonpage@live.co.za>
2019-03-06 21:53:06 +0200

    Made dirt tiles available.

--- resources/availableTiles.js
+++ resources/availableTiles.js
@@ -43,6 +43,51 @@ const availableTiles = [{
     name: "Raised grass",
     sets: ["Nature"],
     image: "/resources/Tiles/Terrain/Grass/grass_17.png"
+},{
+    id: 10,
+    name: "Plain dirt",
+    sets: ["Nature"],
+    image: "/resources/Tiles/Terrain/Dirt/dirt_06.png",
+}, {
+    id: 11,
+    name: "Dirt with few angled trees",
+    sets: ["Nature"],
+    image: "/resources/Tiles/Terrain/Dirt/dirt_11.png"
+}, {
+    id: 12,
+    name: "Dirt with few rounded trees",
+    sets: ["Nature"],
+    image: "/resources/Tiles/Terrain/Dirt/dirt_12.png"
+}, {
+    id: 13,
+    name: "Dirt with many angled trees",
+    sets: ["Nature"],
+    image: "/resources/Tiles/Terrain/Dirt/dirt_13.png"
+}, {
+    id: 14,
+    name: "Dirt with many rounded trees",
+    sets: ["Nature"],
+    image: "/resources/Tiles/Terrain/Dirt/dirt_14.png"
+}, {
+    id: 15,
+    name: "Dirt with few small stones",
+    sets: ["Nature"],
+    image: "/resources/Tiles/Terrain/Dirt/dirt_15.png"
+}, {
+    id: 16,
+    name: "Dirt with many small stones",
+    sets: ["Nature"],
+    image: "/resources/Tiles/Terrain/Dirt/dirt_16.png"
+}, {
+    id: 17,
+    name: "Dirt with small rocks and trees",
+    sets: ["Nature"],
+    image: "/resources/Tiles/Terrain/Dirt/dirt_17.png"
+}, {
+    id: 18,
+    name: "Dirt with big stone and trees",
+    sets: ["Nature"],
+    image: "/resources/Tiles/Terrain/Dirt/dirt_18.png"
 }];
 
 export {
