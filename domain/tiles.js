fecf38a7579c24a40ded0d03526e7b59e295f1f4
Brendon Page <brendonpage@live.co.za>
2019-03-14 19:37:12 +0200

    Split "Nature" tile set into "Grass Land" and "Dirt", added desert tile set.

--- domain/tiles.js
+++ domain/tiles.js
@@ -1,97 +1,145 @@
-const natureTileSet = "Nature";
-const terainGrass = "/resources/Tiles/Terrain/Grass";
-const terainDirt = "/resources/Tiles/Terrain/Dirt";
+const grassLand = "Grass Land";
+const dirt = "Dirt";
+const desert = "Desert";
+const terainGrassPath = "/resources/Tiles/Terrain/Grass";
+const terainDirtPath = "/resources/Tiles/Terrain/Dirt";
+const terainDesertPath = "/resources/Tiles/Terrain/Sand";
 
 const tiles = [{
     id: 1,
     name: "Plain grass",
-    sets: [natureTileSet],
-    image: `${terainGrass}/grass_05.png`,
+    sets: [grassLand],
+    image: `${terainGrassPath}/grass_05.png`,
 }, {
     id: 2,
-    name: "Grass with few angled trees",
-    sets: [natureTileSet],
-    image: `${terainGrass}/grass_10.png`
+    name: "Grass with some angled trees",
+    sets: [grassLand],
+    image: `${terainGrassPath}/grass_10.png`
 }, {
     id: 3,
-    name: "Grass with few rounded trees",
-    sets: [natureTileSet],
-    image: `${terainGrass}/grass_11.png`
+    name: "Grass with some rounded trees",
+    sets: [grassLand],
+    image: `${terainGrassPath}/grass_11.png`
 }, {
     id: 4,
     name: "Grass with many angled trees",
-    sets: [natureTileSet],
-    image: `${terainGrass}/grass_12.png`
+    sets: [grassLand],
+    image: `${terainGrassPath}/grass_12.png`
 }, {
     id: 5,
     name: "Grass with many rounded trees",
-    sets: [natureTileSet],
-    image: `${terainGrass}/grass_13.png`
+    sets: [grassLand],
+    image: `${terainGrassPath}/grass_13.png`
 }, {
     id: 6,
     name: "Grass with big stone",
-    sets: [natureTileSet],
-    image: `${terainGrass}/grass_14.png`
+    sets: [grassLand],
+    image: `${terainGrassPath}/grass_14.png`
 }, {
     id: 7,
     name: "Grass with small stones",
-    sets: [natureTileSet],
-    image: `${terainGrass}/grass_15.png`
+    sets: [grassLand],
+    image: `${terainGrassPath}/grass_15.png`
 }, {
     id: 8,
     name: "Grass with rocks",
-    sets: [natureTileSet],
-    image: `${terainGrass}/grass_16.png`
+    sets: [grassLand],
+    image: `${terainGrassPath}/grass_16.png`
 }, {
     id: 9,
     name: "Raised grass",
-    sets: [natureTileSet],
-    image: `${terainGrass}/grass_17.png`
+    sets: [grassLand],
+    image: `${terainGrassPath}/grass_17.png`
 },{
     id: 10,
     name: "Plain dirt",
-    sets: [natureTileSet],
-    image: `${terainDirt}/dirt_06.png`,
+    sets: [dirt],
+    image: `${terainDirtPath}/dirt_06.png`,
 }, {
     id: 11,
-    name: "Dirt with few angled trees",
-    sets: [natureTileSet],
-    image: `${terainDirt}/dirt_11.png`
+    name: "Dirt with some angled trees",
+    sets: [dirt],
+    image: `${terainDirtPath}/dirt_11.png`
 }, {
     id: 12,
-    name: "Dirt with few rounded trees",
-    sets: [natureTileSet],
-    image: `${terainDirt}/dirt_12.png`
+    name: "Dirt with some rounded trees",
+    sets: [dirt],
+    image: `${terainDirtPath}/dirt_12.png`
 }, {
     id: 13,
     name: "Dirt with many angled trees",
-    sets: [natureTileSet],
-    image: `${terainDirt}/dirt_13.png`
+    sets: [dirt],
+    image: `${terainDirtPath}/dirt_13.png`
 }, {
     id: 14,
     name: "Dirt with many rounded trees",
-    sets: [natureTileSet],
-    image: `${terainDirt}/dirt_14.png`
+    sets: [dirt],
+    image: `${terainDirtPath}/dirt_14.png`
 }, {
     id: 15,
-    name: "Dirt with few small stones",
-    sets: [natureTileSet],
-    image: `${terainDirt}/dirt_15.png`
+    name: "Dirt with some small stones",
+    sets: [dirt],
+    image: `${terainDirtPath}/dirt_15.png`
 }, {
     id: 16,
     name: "Dirt with many small stones",
-    sets: [natureTileSet],
-    image: `${terainDirt}/dirt_16.png`
+    sets: [dirt],
+    image: `${terainDirtPath}/dirt_16.png`
 }, {
     id: 17,
     name: "Dirt with small rocks and trees",
-    sets: [natureTileSet],
-    image: `${terainDirt}/dirt_17.png`
+    sets: [dirt],
+    image: `${terainDirtPath}/dirt_17.png`
 }, {
     id: 18,
     name: "Dirt with big stone and trees",
-    sets: [natureTileSet],
-    image: `${terainDirt}/dirt_18.png`
+    sets: [dirt],
+    image: `${terainDirtPath}/dirt_18.png`
+}, {
+    id: 19,
+    name: "Plain sand",
+    sets: [desert],
+    image: `${terainDesertPath}/sand_07.png`
+}, {
+    id: 20,
+    name: "Sand with some cacti #1",
+    sets: [desert],
+    image: `${terainDesertPath}/sand_12.png`
+}, {
+    id: 21,
+    name: "Sand with some cacti #2",
+    sets: [desert],
+    image: `${terainDesertPath}/sand_13.png`
+}, {
+    id: 21,
+    name: "Sand with many cacti",
+    sets: [desert],
+    image: `${terainDesertPath}/sand_14.png`
+}, {
+    id: 22,
+    name: "Sand with some cacti and a skull",
+    sets: [desert],
+    image: `${terainDesertPath}/sand_15.png`
+}, {
+    id: 23,
+    name: "Sand with some small rocks",
+    sets: [desert],
+    image: `${terainDesertPath}/sand_16.png`
+}, {
+    id: 24,
+    name: "Sand with many small rocks",
+    sets: [desert],
+    image: `${terainDesertPath}/sand_17.png`
+}, {
+    id: 25,
+    name: "Sand with many small rocks and a cactus",
+    sets: [desert],
+    image: `${terainDesertPath}/sand_18.png`
+}, {
+    id: 26,
+    name: "Raised sand",
+    sets: [desert],
+    image: `${terainDesertPath}/sand_19.png`
 }];
 
 export {
